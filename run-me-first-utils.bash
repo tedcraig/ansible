ebe28222c5675473a0ec51feee73b54d3437b505
Ted Craig <tb3034me@gmail.com>
2023-09-27 01:14:37 -0700

    feat: add git install and ansible repo clone to run-me-first-utils.bash

--- run-me-first-utils.bash
+++ run-me-first-utils.bash
@@ -69,11 +69,35 @@ COLOR_RESET="\033[0m"
 
 # install homebrew package manager
 echo -e "${COLOR_BLUE}==>${COLOR_GOLD} install homebrew (includes install of Xcode Command Line Tools):${COLOR_RESET}"
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install ansible cloud automation tool
 echo -e "${COLOR_BLUE}==>${COLOR_GOLD} install ansible:${COLOR_RESET}"
-# brew install ansible
+brew install ansible
+
+# install git
+echo -e "${COLOR_BLUE}==>${COLOR_GOLD} install git:${COLOR_RESET}"
+brew install git
+
+# configure git
+echo -e "${COLOR_BLUE}==>${COLOR_GOLD} configure git:${COLOR_RESET}"
+git config --global user.name "tedcraig"
+git config --global user.email tb3034me@gmail.com
+
+# create scripts directory in home
+echo -e "${COLOR_BLUE}==>${COLOR_GOLD} create scripts directory in home:${COLOR_RESET}"
+mkdir ~/scripts || {
+  echo 'unable to make directory ~/scripts'
+  exit
+}
+
+# clone anisble repo from github
+echo -e "${COLOR_BLUE}==>${COLOR_GOLD} clone ansible repo from github into ~/scripts:${COLOR_RESET}"
+cd ~/scripts || {
+  echo 'unable to change directory into ~/scripts'
+  exit
+}
+git clone https://github.com/tedcraig/ansible.git
 
 echo -e "${COLOR_BLUE}==>${COLOR_GREEN} complete!  ${COLOR_GOLD}Run an ansible playbook for further installations${COLOR_RESET}"
 
